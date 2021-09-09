a88e65acb0963f6a920f77fa1d25cb6a99012b41
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-09 07:53:49 -0300

    feat: :sparkles: add new http status codes and update documentation

--- http-status-codes.js
+++ http-status-codes.js
@@ -1,6 +1,22 @@
+/*
+  Arquivo inspirado no pacote http-status-codes
+  https://www.npmjs.com/package/http-status-codes
+*/
 const StatusCodes = {
   OK: 200,
-  CREATED: 201,  
+  CREATED: 201,
+  NO_CONTENT: 204,
+  MOVED_PERMANENTLY: 301,
+  NOT_MODIFIED: 304,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  REQUEST_TIMEOUT: 408,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
 };
 
 /*
