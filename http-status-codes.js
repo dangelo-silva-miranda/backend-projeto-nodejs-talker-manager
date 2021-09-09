1314ca2c9a9ffcfa54dce9f8620be777fef14ad2
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-09 07:34:27 -0300

    refactor: :recycle: create http-status-codes.js file and refactor index.js

---
+++ http-status-codes.js
@@ -0,0 +1,12 @@
+const StatusCodes = {
+  OK: 200,
+  CREATED: 201,  
+};
+
+/*
+  Material consultado sobre exports
+  https://www.freecodecamp.org/news/node-module-exports-explained-with-javascript-export-function-examples/
+*/
+module.exports = {
+  StatusCodes,
+};
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { StatusCodes } = require('./http-status-codes');
+
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(StatusCodes.OK).send();
 });
 
 app.listen(PORT, () => {
