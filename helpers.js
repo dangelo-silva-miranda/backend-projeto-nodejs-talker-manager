f3ea87eefbf09e66c25c2b542b9e597b305a64c9
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-19 22:26:07 -0300

    feat: :sparkles: implements helpers functions

---
+++ helpers.js
@@ -0,0 +1,38 @@
+/*
+Material consultado sobre validação de e-mail com regex
+https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+
+Material consultado sobre como testar texto com regex
+https://www.w3schools.com/js/js_regexp.asp
+*/
+const isValidPattern = (field, pattern) => pattern.test(field);
+
+const hasInfo = (field) => field && field !== '';
+const hasMininumSize = (field, size) => field >= size;
+
+// const validateField = (field) => {
+//   const message = null;
+//   const { name, value } = field;
+  
+//   if (!hasInfo(value)) {
+//     return { message: `O campo "${name}" é obrigatório` };
+//   }
+
+//   if (name !== 'email') {
+//     const { restrictions: { minimumSize } } = field;
+//     if (value.length < minimumSize) {
+//       return { message: `O "${name}" deve ter pelo menos ${minimumSize} caracteres` };
+//     }    
+//   } else if (!isValidEmail(value)) {
+//       return { message: 'O "email" deve ter o formato "email@email.com"' };
+//     }
+
+//   return message;
+// };
+
+module.exports = {
+  hasInfo,
+  hasMininumSize,
+  isValidPattern,
+  // validateField,
+};
