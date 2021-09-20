302aa421d2d1924ffbe8bb2b9d84430b35fb02e0
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-20 00:49:53 -0300

    feat: :sparkles: implements validateName, validateAge, validateTalk, validateRate and validateWatchedAt as middleware and changes validateToken

--- middleware/validations.js
+++ middleware/validations.js
@@ -1,4 +1,4 @@
-const { hasMininumSize, hasInfo, isValidPattern } = require('../helpers');
+const { hasMininum, hasInfo, isValidPattern } = require('../helpers');
 const { StatusCodes } = require('../http-status-codes');
 
 const validateEmail = (req, res, next) => {
@@ -30,7 +30,7 @@ const validatePassword = (req, res, next) => {
   }
 
   const minimumCharacterSize = 6;
-  if (!hasMininumSize(password.length, minimumCharacterSize)) {
+  if (!hasMininum(password.length, minimumCharacterSize)) {
     return res.status(StatusCodes.BAD_REQUEST).json({ 
       message: 'O "password" deve ter pelo menos 6 caracteres', 
     });
@@ -48,9 +48,90 @@ const validateToken = (req, res, next) => {
     });
   }
   const minimumCharacterSize = 16;
-  if (!hasMininumSize(token, minimumCharacterSize)) {
+  if (token.length !== minimumCharacterSize) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       message: 'Token inválido',
+    });
+  }
+
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name = '' } = req.body;
+
+  if (!hasInfo(name)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O campo "name" é obrigatório',
+    }); 
+  }
+
+  const minimumCharacterSize = 3;
+  if (!hasMininum(name.length, minimumCharacterSize)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age = '' } = req.body;
+  
+  if (!hasInfo(age)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O campo "age" é obrigatório',
+    }); 
+  }
+
+  const minimumAge = 18;
+  if (!hasMininum(age, minimumAge)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    }); 
+  }
+
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk = '' } = req.body;
+  
+  if (!hasInfo(talk) || !hasInfo(talk.watchedAt) || !hasInfo(talk.rate)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    }); 
+  }
+
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk: { rate = '' } } = req.body;
+
+  const ratePattern = /^([1-5])$/;
+  if (!isValidPattern(rate, ratePattern)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  next();
+};
+
+/*
+  Material consultado sobre regex para o formato de data dd-mm-yyyy
+  https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy-with-leap-year-support
+*/
+const validateWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt = '' } } = req.body;
+  
+  const watchedAtPattern = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+  if (!watchedAt.match(watchedAtPattern)) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
   }
 
@@ -61,4 +142,9 @@ module.exports = {
   validateToken,
   validateEmail,
   validatePassword,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateRate,
+  validateWatchedAt,
 };
